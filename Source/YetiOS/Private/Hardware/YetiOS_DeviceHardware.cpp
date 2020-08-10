9df78e2e75dbd46e3bddba24da66de999e806643
ryanjon2040 <mindfreak2040@gmail.com>
2020-08-10 13:03:42 +0530

    New device hardware class.

---
+++ Source/YetiOS/Private/Hardware/YetiOS_DeviceHardware.cpp
@@ -0,0 +1,9 @@
+// Copyright 2019 YetiTech Studios, Pvt Ltd. All Rights Reserved.
+
+
+#include "Hardware/YetiOS_DeviceHardware.h"
+
+UYetiOS_DeviceHardware::UYetiOS_DeviceHardware()
+{
+	CurrentTemperature = 0;
+}
---
+++ Source/YetiOS/Public/Hardware/YetiOS_DeviceHardware.h
@@ -0,0 +1,31 @@
+// Copyright 2019 YetiTech Studios, Pvt Ltd. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Hardware/YetiOS_BaseHardware.h"
+#include "YetiOS_DeviceHardware.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YETIOS_API UYetiOS_DeviceHardware : public UYetiOS_BaseHardware
+{
+	GENERATED_BODY()
+
+protected:
+
+	/* Temperature settings. */
+	UPROPERTY(EditDefaultsOnly, Category = "Hardware")
+	FYetiOSTemperature Temperature;
+
+	/* Current temperature of this hardware. */
+	UPROPERTY(VisibleInstanceOnly, Category = Debug)
+	uint8 CurrentTemperature;
+
+public:
+
+	UYetiOS_DeviceHardware();
+	
+};
