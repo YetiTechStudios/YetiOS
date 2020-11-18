27abe7e7bc4fd10de50810ace784cc2b385d6b92
ryanjon2040 <mindfreak2040@gmail.com>
2020-11-18 18:37:13 +0530

    Call `ProgramInstalled` method from OS.

--- Source/YetiOS/Private/Core/YetiOS_Core.cpp
+++ Source/YetiOS/Private/Core/YetiOS_Core.cpp
@@ -318,6 +318,7 @@ UYetiOS_BaseProgram* UYetiOS_Core::InstallProgram(TSubclassOf<UYetiOS_BaseProgra
 		const FText Description = FText::Format(LOCTEXT("YetiOS_InstallProgramUnknownErrorDescription", "{0} installed successfuly."), MyProgramName);
 		const FYetiOsNotification NewNotification = FYetiOsNotification(EYetiOsNotificationCategory::CATEGORY_App, Title, Description, FText::FromString("INSTALL_SUCCESS"), EYetiOsNotificationType::TYPE_Info);
 		CreateOsNotification(NewNotification);
+		NewProgram->ProgramInstalled();
 		OnProgramInstalled.Broadcast(NewProgram);
 		return NewProgram;
 	}
