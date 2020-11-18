6820a334b8e8f279a2c05488a2d8d22c9b4169b6
ryanjon2040 <mindfreak2040@gmail.com>
2020-11-18 18:39:04 +0530

    Inherit `UYetiOS_AppIconWidget` from Icon Widget class.

--- Source/YetiOS/Private/Widgets/YetiOS_AppIconWidget.cpp
+++ Source/YetiOS/Private/Widgets/YetiOS_AppIconWidget.cpp
@@ -14,39 +14,28 @@
 
 UYetiOS_AppIconWidget::UYetiOS_AppIconWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	DoubleClickTime = 0.2;
 	BaseProgram = nullptr;
 }
 
-UYetiOS_AppIconWidget* UYetiOS_AppIconWidget::CreateProgramIconWidget(const UObject* WorldContextObject, class UYetiOS_BaseProgram* InCreatedProgram, FYetiOsError& OutErrorMessage)
+UYetiOS_AppIconWidget* UYetiOS_AppIconWidget::CreateProgramIconWidget(class UYetiOS_BaseProgram* InCreatedProgram, FYetiOsError& OutErrorMessage)
 {
 	const UYetiOS_BaseDevice* MyDevice = InCreatedProgram->GetOwningOS()->GetOwningDevice();
 	UYetiOS_Core* MyOS = MyDevice->GetOperatingSystem();
 	if (MyOS && !MyOS->IsPendingKill())
 	{
-		APlayerController* MyController = UGameplayStatics::GetPlayerController(WorldContextObject, 0);
+		APlayerController* MyController = UGameplayStatics::GetPlayerController(InCreatedProgram, 0);
 		UYetiOS_AppIconWidget* ProxyWidget = CreateWidget<UYetiOS_AppIconWidget>(MyController, InCreatedProgram->GetProgramIconWidgetClass());
-		ProxyWidget->OwningOS = MyOS;
+		ProxyWidget->SetOperatingSystem(MyOS);
 		ProxyWidget->BaseProgram = InCreatedProgram;
 		ProxyWidget->ProgramClass = InCreatedProgram->GetClass();
 		return ProxyWidget;
 	}
 
 	OutErrorMessage.ErrorCode = LOCTEXT("Program_CreateWidgetErrorCode", "ERR_NO_OS");
-	OutErrorMessage.ErrorException = LOCTEXT("Program_CreateWidgetErrorException", "Cannot create program widget.");
-	OutErrorMessage.ErrorDetailedException = LOCTEXT("Program_CreateWidgetErrorDetailedException", "Failed to create program widget. An Operating System is required.");
+	OutErrorMessage.ErrorException = LOCTEXT("Program_CreateWidgetErrorException", "Cannot create program icon widget.");
+	OutErrorMessage.ErrorDetailedException = LOCTEXT("Program_CreateWidgetErrorDetailedException", "Failed to create program icon widget. An Operating System is required.");
 
 	return nullptr;
-}
-
-void UYetiOS_AppIconWidget::CheckForDoubleClick()
-{
-	NumberOfClicks++;
-	FTimerManager& MyTimerManager = GetWorld()->GetTimerManager();
-	if (MyTimerManager.IsTimerActive(TimerHandle_DoubleClick) == false)
-	{
-		MyTimerManager.SetTimer(TimerHandle_DoubleClick, this, &UYetiOS_AppIconWidget::Internal_OnDoubleClick, DoubleClickTime, false);
-	}
 }
 
 void UYetiOS_AppIconWidget::StartProgram(FYetiOsError& OutErrorMessage)
@@ -64,16 +53,6 @@ void UYetiOS_AppIconWidget::StartProgram(FYetiOsError& OutErrorMessage)
 		FYetiOsNotification NewNotification = FYetiOsNotification(EYetiOsNotificationCategory::CATEGORY_App, OutErrorMessage.ErrorException, OutErrorMessage.ErrorDetailedException, OutErrorMessage.ErrorCode, nullptr, EYetiOsNotificationType::TYPE_Error, 5.f);
 		OwningOS->CreateOsNotification(NewNotification);
 	}
-}
-
-void UYetiOS_AppIconWidget::Internal_OnDoubleClick()
-{
-	if (NumberOfClicks >= 2)
-	{
-		K2_OnDoubleClick();
-	}
-
-	NumberOfClicks = 0;
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/YetiOS/Public/Widgets/YetiOS_AppIconWidget.h
+++ Source/YetiOS/Public/Widgets/YetiOS_AppIconWidget.h
@@ -3,56 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "YetiOS_Types.h"
+#include "Widgets/YetiOS_IconWidget.h"
 #include "YetiOS_AppIconWidget.generated.h"
 
 /**
  * 
  */
 UCLASS(Abstract)
-class YETIOS_API UYetiOS_AppIconWidget : public UUserWidget
+class YETIOS_API UYetiOS_AppIconWidget : public UYetiOS_IconWidget
 {
 	GENERATED_BODY()
 	
-	FTimerHandle TimerHandle_DoubleClick;
-	
 private:
-
-	UPROPERTY(EditAnywhere, Category = "Yeti OS App Icon Widget")
-	float DoubleClickTime;
 
 	UPROPERTY(EditAnywhere, Category = "Yeti OS App Icon Widget", BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<class UYetiOS_BaseProgram> ProgramClass;
 
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Debug, meta = (AllowPrivateAccess = "true"))
-	class UYetiOS_Core* OwningOS;
-
-	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Debug, meta = (AllowPrivateAccess = "true"))
 	class UYetiOS_BaseProgram* BaseProgram;
-
-	uint8 NumberOfClicks;
 	
 public:
 
 	UYetiOS_AppIconWidget(const FObjectInitializer& ObjectInitializer);
 
-	static UYetiOS_AppIconWidget* CreateProgramIconWidget(const UObject* WorldContextObject, class UYetiOS_BaseProgram* InCreatedProgram, FYetiOsError& OutErrorMessage);
+	static UYetiOS_AppIconWidget* CreateProgramIconWidget(class UYetiOS_BaseProgram* InCreatedProgram, FYetiOsError& OutErrorMessage);
 	
 public:
 
-	UFUNCTION(BlueprintCallable, Category = "Yeti OS App Icon")
-	void CheckForDoubleClick();
-
-	UFUNCTION(BlueprintCallable, Category = "Yeti OS App Icon")
+	UFUNCTION(BlueprintCallable, Category = "Yeti OS App Icon Widget")
 	void StartProgram(FYetiOsError& OutErrorMessage);
 
-private:
-
-	void Internal_OnDoubleClick();
-	
-protected:
-
-	UFUNCTION(BlueprintImplementableEvent, Category = "Yeti OS App Icon", DisplayName = "OnDoubleClick")
-	void K2_OnDoubleClick();
 };
