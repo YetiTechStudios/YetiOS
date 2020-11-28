f00342f3eb0438c6b0e18207ba0f91956c6cd4e0
ryanjon2040 <mindfreak2040@gmail.com>
2020-11-28 13:56:11 +0530

    Register factory classes in Content Browser.

--- Source/YetiOSEditor/Private/YetiOSEditorModule.cpp
+++ Source/YetiOSEditor/Private/YetiOSEditorModule.cpp
@@ -3,7 +3,13 @@
 #include "YetiOSEditorModule.h"
 #include "Editor/LevelEditor/Public/LevelEditor.h"
 #include "YetiOSEditorCommands.h"
+#include "IAssetTools.h"
+#include "AssetToolsModule.h"
+#include "IAssetTypeActions.h"
+#include "YetiOS_Factories.h"
 #include "YetiOS_ThumbnailRenderer.h"
+#include "Core/YetiOS_DirectoryBase.h"
+
 DEFINE_LOG_CATEGORY_STATIC(LogYetiOs, All, All)
 
 #define printlog(Param1)				UE_LOG(LogYetiOs, Log, TEXT("%s"), *FString(Param1))
@@ -18,6 +24,28 @@ void FYetiOSEditorModule::StartupModule()
 
 	YetiOsCommands = Internal_CreateCommands();
 	Internal_CreateMenuBarEntry();
+	IAssetTools& AssetTools = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools").Get();
+	YetiOS_AssetCategory = AssetTools.RegisterAdvancedAssetCategory(FName(TEXT("YetiOsCategory")), LOCTEXT("YetiOsCategory", "Operating System Simulator"));
+
+	TSharedRef<IAssetTypeActions> Category_BaseDirectory = MakeShareable(new FAssetTypeActions_BaseDirectory);
+	TSharedRef<IAssetTypeActions> Category_BaseFile = MakeShareable(new FAssetTypeActions_BaseFile);
+	TSharedRef<IAssetTypeActions> Category_BaseProgram = MakeShareable(new FAssetTypeActions_BaseProgram);
+	TSharedRef<IAssetTypeActions> Category_OS = MakeShareable(new FAssetTypeActions_OperatingSystem);
+	TSharedRef<IAssetTypeActions> Category_PortableDevice = MakeShareable(new FAssetTypeActions_PortableDevice);
+	TSharedRef<IAssetTypeActions> Category_StationaryDevice = MakeShareable(new FAssetTypeActions_StationaryDevice);
+	TSharedRef<IAssetTypeActions> Category_DeviceManager = MakeShareable(new FAssetTypeActions_DeviceManager);
+	TSharedRef<IAssetTypeActions> Category_ProgramsRepository = MakeShareable(new FAssetTypeActions_ProgramsRepository);
+
+	AssetTools.RegisterAssetTypeActions(Category_BaseDirectory);
+	AssetTools.RegisterAssetTypeActions(Category_BaseFile);
+	AssetTools.RegisterAssetTypeActions(Category_BaseProgram);
+	AssetTools.RegisterAssetTypeActions(Category_OS);
+	AssetTools.RegisterAssetTypeActions(Category_PortableDevice);
+	AssetTools.RegisterAssetTypeActions(Category_StationaryDevice);
+	AssetTools.RegisterAssetTypeActions(Category_DeviceManager);
+	AssetTools.RegisterAssetTypeActions(Category_ProgramsRepository);
+	
+	printlog("Registered Content Browser extensions.");
 }
 
 void FYetiOSEditorModule::ShutdownModule()
@@ -51,6 +79,8 @@ void FYetiOSEditorModule::Internal_CreateMenuBarEntry()
 				FNewMenuDelegate::CreateStatic(AddMenuExtension), "YetiOSEditorPlugin");
 		}));
 		LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
+
+		printlog("Registered Menubar.");
 	}
 }
 
@@ -89,9 +119,12 @@ TSharedPtr<FUICommandList> FYetiOSEditorModule::Internal_CreateCommands()
 		FPlatformProcess::LaunchURL(TEXT("https://www.unrealengine.com/marketplace/profile/YetiTech+Studios"), NULL, NULL);
 	}), FCanExecuteAction());
 
+	printlog("Registered FUICommandList for YetiOS Commands.");
 	return Local_YetiOsCommands;
 }
 
 IMPLEMENT_MODULE(FYetiOSEditorModule, YetiOSEditor)
 
 #undef LOCTEXT_NAMESPACE
+
+#undef printlog
